51b17ef3a97c2b75794fa53a4b4a66e27e2bc4f8
Ivan Vucicevic <ivucicev@Ivans-MacBook-Pro.local>
2025-03-01 10:29:48 +0100

    fix load langauges dynamically without bulk import [ci release]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,63 +6,55 @@ import { Store } from '@ngrx/store';
 import { LAYOUT_TYPES } from './store/layouts/layout';
 import { LayoutState } from './store/layouts/layout-reducers';
 
-
-import hrMessages from "../assets/i18n/messages/hr.json";
-import deMessages from "devextreme/localization/messages/de.json";
-import enMessages from "devextreme/localization/messages/en.json";
-import esMessages from "devextreme/localization/messages/es.json";
-import itMessages from "devextreme/localization/messages/it.json";
-import frMessages from "devextreme/localization/messages/fr.json";
-import plMessages from "devextreme/localization/messages/pl.json";
-
 import { loadMessages } from "devextreme/localization";
 
 @Component({
-	selector: 'eenvo-root',
-	standalone: true,
-	imports: [RouterOutlet],
-	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss'
+    selector: 'eenvo-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-	layOutTypes = LAYOUT_TYPES
+    layOutTypes = LAYOUT_TYPES
 
-	layOutData!: LayoutState;
+    layOutData!: LayoutState;
 
-	constructor(private store: Store<RootReducerState>) {
+    constructor(private store: Store<RootReducerState>) {
 
-        loadMessages(hrMessages)
-        loadMessages(deMessages)
-        loadMessages(enMessages)
-        loadMessages(esMessages)
-        loadMessages(itMessages)
-        loadMessages(frMessages)
-        loadMessages(plMessages)
+        const lang =localStorage.getItem('lang') || 'en';
+        if (lang == 'hr') {
+            import("../assets/i18n/messages/hr.json").then(res => {
+                loadMessages(res)
+            });
+        } else {
+            import(`../../node_modules/devextreme/localization/messages/${lang}.json`).then(res => {
+                loadMessages(res)
+            });
+        }
 
-		this.store.select('layout').subscribe((data) => {
-			this.layOutData = data;
-		})
+        this.store.select('layout').subscribe((data) => {
+            this.layOutData = data;
+        })
+
+    }
+
+    ngOnInit(): void {
+        this.changeMode(this.layOutData.LAYOUT_MODE);
+        this.changeLayout(this.layOutData.LAYOUT)
+    }
 
 
+    ngOnDestroy(): void { }
 
-	}
+    // Mode Change
+    changeMode(mode: string) {
+        this.store.dispatch(changeMode({ mode }));
+    }
 
-	ngOnInit(): void {
-		this.changeMode(this.layOutData.LAYOUT_MODE);
-		this.changeLayout(this.layOutData.LAYOUT)
-	}
-
-
-	ngOnDestroy(): void { }
-
-	// Mode Change
-	changeMode(mode: string) {
-		this.store.dispatch(changeMode({ mode }));
-	}
-
-	changeLayout(layout: string) {
-		this.store.dispatch(changelayout({ layout }));
-	}
+    changeLayout(layout: string) {
+        this.store.dispatch(changelayout({ layout }));
+    }
 
 }
 
