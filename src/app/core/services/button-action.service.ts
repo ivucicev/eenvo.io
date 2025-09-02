fbd079f56a45431e8fcadd8e998557bdf0852955
ivucicev <ivucicev@gmail.com>
2025-09-02 15:17:51 +0200

    fix removed actions that are not implemented from invoice && fixed spacing [ci release]

--- src/app/core/services/button-action.service.ts
+++ src/app/core/services/button-action.service.ts
@@ -2,31 +2,31 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 export interface ButtonAction {
-  id: string;
-  label: string;
-  icon?: string;
-  class?: string;
-  action: () => void;
+	id: string;
+	label: string;
+	icon?: string;
+	class?: string;
+	action: () => void;
 }
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ButtonActionService {
-  private actionsSubject = new BehaviorSubject<ButtonAction[]>([]);
-  actions$ = this.actionsSubject.asObservable();
+	private actionsSubject = new BehaviorSubject<ButtonAction[]>([]);
+	actions$ = this.actionsSubject.asObservable();
 
-  addAction(action: ButtonAction) {
-    const currentActions = this.actionsSubject.getValue();
-    this.actionsSubject.next([...currentActions, action]);
-  }
+	addAction(action: ButtonAction) {
+		const currentActions = this.actionsSubject.getValue();
+		this.actionsSubject.next([...currentActions, action]);
+	}
 
-  removeAction(actionId: string) {
-    const currentActions = this.actionsSubject.getValue();
-    this.actionsSubject.next(currentActions.filter(action => action.id !== actionId));
-  }
+	removeAction(actionId: string) {
+		const currentActions = this.actionsSubject.getValue();
+		this.actionsSubject.next(currentActions.filter(action => action.id !== actionId));
+	}
 
-  clearActions() {
-    this.actionsSubject.next([]);
-  }
+	clearActions() {
+		this.actionsSubject.next([]);
+	}
 }
